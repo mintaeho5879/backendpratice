a7ec97a33c21e3d4686e61581e7c332553c5de61
minbonjwa <minbonjwa@gmail.com>
2025-07-09 18:02:04 +0900

    갤러리 주문하기

--- src/main/java/kr/co/wikibook/gallery/common/WebMvcConfiguration.java
+++ src/main/java/kr/co/wikibook/gallery/common/WebMvcConfiguration.java
@@ -1,12 +1,27 @@
 package kr.co.wikibook.gallery.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Slf4j
+@Configuration //빈등록 Spring Container가 객체생성을 대신 해줌.
 public class WebMvcConfiguration implements WebMvcConfigurer{
 
+    private final String uploadPath;
+
+    public WebMvcConfiguration(@Value("${constants.file.directory}") String uploadPath){
+        this.uploadPath = uploadPath;
+        log.info("Upload Path: {}", uploadPath);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pic/**")
+                .addResourceLocations("file:"+uploadPath);
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
